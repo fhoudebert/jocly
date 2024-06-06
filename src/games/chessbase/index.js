90b9660bdba4fd053c7e4b4a0818ca606416a472
fhoudebert <fhoudebert@gmail.com>
2024-06-06 17:19:58 +0200

    Add variant : Gross chess

--- src/games/chessbase/index.js
+++ src/games/chessbase/index.js
@@ -511,6 +511,12 @@ exports.games = (function () {
 		"locust-move-model.js",
 		"minjiku-shogi-model.js"
 	]
+	var modelScripts_kyoto = [
+		"base-model.js",
+		"grid-geo-model.js",
+		"drop-model.js",
+		"shogi/kyoto-shogi-model.js"
+	]
 	var config_model_levels_11 = {
 		"name": "easy",
 		"label": "Easy",
@@ -558,12 +564,6 @@ exports.games = (function () {
 		"maxNodes": 60000,
 		"maxDuration": 45
 	}
-	var modelScripts_kyoto = [
-		"base-model.js",
-		"grid-geo-model.js",
-		"drop-model.js",
-		"shogi/kyoto-shogi-model.js"
-	]
 	var config_model_levels_15 = [
 		config_model_levels_11,
 		config_model_levels_12,
@@ -1394,6 +1394,12 @@ exports.games = (function () {
         "fairy-piece-model.js",
         "prelude-model.js",
  		"duodecimal/timurid-model.js"
+ 	]
+	var modelScripts_gross = [
+ 		"base-model.js",
+ 		"grid-geo-model.js",
+        "fairy-piece-model.js",
+ 		"duodecimal/gross-model.js"
  	]
  	var config_view_js_timurid = [
  		"base-view.js",
@@ -6001,6 +6007,121 @@ exports.games = (function () {
 			},
 			"viewScripts": config_view_js_gigaII
 		},
+		{
+ 			"name": "gross-chess",
+ 			"modelScripts": modelScripts_gross,
+ 			"config": {
+ 				"status": true,
+ 				"model": {
+ 					"title-en": "Gross Chess",
+
+ 					"summary": "Omega/Gothic/Cambaluc Chess on 12x12",
+ 					"rules": {
+ 						"en": "res/rules/duodecimal/gross-rules.html",
+						"fr": "res/rules/duodecimal/gross-rules_fr.html"
+ 					},
+ 					"module": "chessbase",
+ 					"plazza": "true",
+ 					"thumbnail": "res/rules/duodecimal/gross-thumb.png",
+ 					"released": 1497874349,
+
+ 					"credits": {
+ 						"en": "res/rules/duodecimal/gross-credits.html"
+ 					},
+ 					"gameOptions": config_model_gameOptions,
+ 					"obsolete": false,
+ 					"js": modelScripts_gross,
+
+ 					"description": {
+ 						"en": "res/rules/duodecimal/gross-description.html"
+ 					},
+ 					"levels": config_model_levels_15
+ 				},
+ 				"view": {
+ 					"title-en": "Gross view",
+ 					"visuals": {
+ 						"600x600": [
+ 							"res/visuals/gross-600x600-3d.jpg",
+ 							"res/visuals/gross-600x600-2d.jpg"
+ 						]
+ 					},
+ 					"xdView": true,
+ 					"css": config_view_css,
+ 					"preferredRatio": 1,
+ 					"useShowMoves": true,
+ 					"useNotation": true,
+ 					"module": "chessbase",
+ 					"defaultOptions": config_view_defaultOptions,
+
+ 					"skins": [
+ 						{
+ 							"name": "skin3d",
+ 							"title": "3D Classic",
+ 							"3d": true,
+ 							"preload": [
+ 								"smoothedfilegeo|0|/res/ring-target.js",
+ 								"image|/res/images/cancel.png",
+ 								"image|/res/images/wikipedia.png",
+
+ 								"smoothedfilegeo|0|/res/fairy/pawn/pawn.js",
+ 								"image|/res/fairy/pawn/pawn-diffusemap.jpg",
+ 								"image|/res/fairy/pawn/pawn-normalmap.jpg",
+ 								"smoothedfilegeo|0|/res/fairy/rook/rook.js",
+ 								"image|/res/fairy/rook/rook-diffusemap.jpg",
+ 								"image|/res/fairy/rook/rook-normalmap.jpg",
+
+ 								"smoothedfilegeo|0|/res/fairy/bishop/bishop.js",
+ 								"image|/res/fairy/bishop/bishop-diffusemap.jpg",
+ 								"image|/res/fairy/bishop/bishop-normalmap.jpg",
+ 								"smoothedfilegeo|0|/res/fairy/knight/knight.js",
+ 								"image|/res/fairy/knight/knight-diffusemap.jpg",
+ 								"image|/res/fairy/knight/knight-normalmap.jpg",
+
+ 								"smoothedfilegeo|0|/res/fairy/queen/queen.js",
+ 								"image|/res/fairy/queen/queen-diffusemap.jpg",
+ 								"image|/res/fairy/queen/queen-normalmap.jpg",
+ 								"smoothedfilegeo|0|/res/fairy/king/king.js",
+ 								"image|/res/fairy/king/king-diffusemap.jpg",
+
+ 								"image|/res/fairy/king/king-normalmap.jpg",
+								"smoothedfilegeo|0|/res/fairy/prince/prince.js",
+								"image|/res/fairy/prince/prince-diffusemap.jpg",
+								"image|/res/fairy/prince/prince-normalmap.jpg",
+ 								"smoothedfilegeo|0|/res/fairy/elephant/elephant.js",
+ 								"image|/res/fairy/elephant/elephant-diffusemap.jpg",
+
+ 								"image|/res/fairy/elephant/elephant-normalmap.jpg",
+ 								"smoothedfilegeo|0|/res/fairy/cannon2/cannon2.js",
+ 								"image|/res/fairy/cannon2/cannon2-diffusemap.jpg",
+ 								"image|/res/fairy/cannon2/cannon2-normalmap.jpg",
+ 								"smoothedfilegeo|0|/res/fairy/griffon/griffon.js",
+ 								"image|/res/fairy/griffon/griffon-diffusemap.jpg",
+
+ 								"image|/res/fairy/griffon/griffon-normalmap.jpg",
+ 								"smoothedfilegeo|0|/res/fairy/lion/lion.js",
+								"image|/res/fairy/lion/lion-diffusemap.jpg",
+								"image|/res/fairy/lion/lion-normalmap.jpg",
+ 								"smoothedfilegeo|0|/res/fairy/camel/camel.js",
+
+ 								"image|/res/fairy/camel/camel-diffusemap.jpg",
+ 								"image|/res/fairy/camel/camel-normalmap.jpg"							      
+ 							],
+ 							"world": config_view_skins_world,
+ 							"camera": config_view_skins_camera
+ 						},
+
+ 						config_view_skins_9
+ 					],
+ 					"animateSelfMoves": false,
+ 					"switchable": true,
+ 					"sounds": config_view_sounds,
+ 					"js": config_view_js_duodecimal,
+
+ 					"useAutoComplete": true
+ 				}
+ 			},
+ 			"viewScripts": config_view_js_duodecimal
+  		},
 		{
  			"name": "timurid-chess",
  			"modelScripts": modelScripts_timurid,
